129297a87727d7bf7673f626ecc6f385c2ef1751
José792 <joseph_camilo@ucne.edu.do>
2022-06-05 23:12:32 -0400

    Agregando Proyecto

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Name
+{
+    class Program
+    {
+        public static double fact(int num)
+        {
+            if (num == 0 || num == 1)
+                return 1;
+            return num * fact(num - 1);
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Digite el valor de n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("El factorial de {0} es: {1}", n, fact(n));
+            Console.ReadKey();
+        }
+    }
+}
